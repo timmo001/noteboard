8323665f7e1d4a0303b880dd25a45471430d18dd
Timmo <contact@timmo.xyz>
2019-03-04 23:11:21 +0000

    :sparkles: Add authentication and login screen

--- src/Components/Root.js
+++ src/Components/Root.js
@@ -2,8 +2,13 @@ import React from 'react';
 import moment from 'moment';
 import SunCalc from 'suncalc';
 import PropTypes from 'prop-types';
+import feathers from '@feathersjs/feathers';
+import socketio from '@feathersjs/socketio-client';
+import io from 'socket.io-client';
+import auth from '@feathersjs/authentication-client';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Snackbar from '@material-ui/core/Snackbar';
+import Login from './Login';
 import Notes from './Notes';
 
 const styles = theme => ({
@@ -21,6 +26,7 @@ class Root extends React.PureComponent {
   state = {
     snackMessage: { open: false, text: '' },
     connected: false,
+    loggedIn: false,
     notes: [
       {
         note: 'Hello World!',
@@ -47,9 +53,11 @@ class Root extends React.PureComponent {
     ]
   };
 
-  componentDidMount = () =>
+  componentDidMount = () => {
     navigator.geolocation &&
-    navigator.geolocation.getCurrentPosition(this.setTheme);
+      navigator.geolocation.getCurrentPosition(this.setTheme);
+    this.login();
+  };
 
   setTheme = position =>
     moment(
@@ -62,15 +70,63 @@ class Root extends React.PureComponent {
       ? this.props.setTheme(1)
       : this.props.setTheme(0);
 
+  login = (data = undefined) => {
+    const socket = io(
+      `${window.location.protocol}//${window.location.hostname}:${process.env
+        .REACT_APP_API_PORT || 3030}`
+    );
+    const app = feathers();
+
+    // Setup the transport (Rest, Socket, etc.) here
+    app.configure(socketio(socket));
+
+    // Available options are listed in the "Options" section
+    app.configure(auth({ storage: localStorage }));
+
+    if (!data)
+      app.passport.getJWT().then(accessToken => {
+        accessToken &&
+          this.authenticate(app, {
+            strategy: 'jwt',
+            accessToken
+          });
+      });
+    else this.authenticate(app, data);
+  };
+
+  authenticate = (app, data) => {
+    console.log(data);
+    app
+      .authenticate(data)
+      .then(response => {
+        console.log('Authenticated!', response);
+        app.passport.verifyJWT(response.accessToken);
+        this.setState({ loggedIn: true });
+      })
+      .catch(e => {
+        console.error('Authentication error:', e);
+        this.setState({ loginError: e.message }, () =>
+          setTimeout(
+            () => this.setState({ loggedIn: false, loginError: undefined }),
+            10000
+          )
+        );
+      });
+  };
+
   updateNote = note => console.log('Update Note:', note);
 
   render() {
     const { classes } = this.props;
-    const { snackMessage, notes } = this.state;
+    const { snackMessage, loggedIn, loginError, notes } = this.state;
 
     return (
       <div className={classes.root}>
-        <Notes notes={notes} updateNote={this.updateNote} />
+        {loggedIn ? (
+          <Notes notes={notes} updateNote={this.updateNote} />
+        ) : (
+          <Login login={this.login} loginError={loginError} />
+        )}
         <Snackbar
           open={snackMessage.open}
           autoHideDuration={!snackMessage.persistent ? 4000 : null}
