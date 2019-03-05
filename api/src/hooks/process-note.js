eb3eb1db9acc256dd4651b2b5cdb1d320b9959a4
Timmo <contact@timmo.xyz>
2019-03-05 13:13:29 +0000

    :hammer: Add validation for notes

--- api/src/hooks/process-note.js
+++ api/src/hooks/process-note.js
@@ -5,21 +5,34 @@ module.exports = function(_options = {}) {
   // eslint-disable-line no-unused-vars
   return async context => {
     const { data } = context;
+    const { x, y, background, color, size, text } = data;
 
-    // Throw an error if we didn't get a text
-    if (!data.text) {
-      throw new Error('A note must have text');
-    }
+    // Throw an error if we didn't get a x position
+    if (!x) throw new Error('A note must have a x position');
+
+    // Throw an error if we didn't get a y position
+    if (!y) throw new Error('A note must have a y position');
+
+    // Throw an error if we didn't get any text
+    if (!text) throw new Error('A note must have text');
+
+    // Number validation
+    if (!Number.isInteger(x))
+      throw new Error('The x position must be a number');
+    if (!Number.isInteger(y))
+      throw new Error('The y position must be a number');
+    if (!Number.isInteger(size)) throw new Error('The size must be a number');
 
     // The authenticated user
     const user = context.params.user;
-    // The actual message text
-    const text = context.data.text
-      // Messages can't be longer than 400 characters
-      .substring(0, 400);
 
     // Override the original data (so that people can't submit additional stuff)
     context.data = {
+      x,
+      y,
+      background,
+      color,
+      size,
       text,
       // Set the user id
       userId: user._id,
