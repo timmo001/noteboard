75efa44fea8837b5336fb3ee981166f7f61f617f
Timmo <contact@timmo.xyz>
2019-03-07 16:53:42 +0000

    :pencil2: Write tests

--- api/test/hooks/process-note.test.js
+++ api/test/hooks/process-note.test.js
@@ -2,26 +2,50 @@ const assert = require('assert');
 const feathers = require('@feathersjs/feathers');
 const processNote = require('../../src/hooks/process-note');
 
-describe('\'process-note\' hook', () => {
+describe('process-note hook', () => {
   let app;
 
   beforeEach(() => {
+    // Create a new plain Feathers application
     app = feathers();
 
-    app.use('/dummy', {
-      async get(id) {
-        return { id };
+    // Register a dummy custom service that just return the
+    // note data back
+    app.use('/notes', {
+      async create(data) {
+        return data;
       }
     });
 
-    app.service('dummy').hooks({
-      before: processNote()
+    // Register the `processnote` hook on that service
+    app.service('notes').hooks({
+      before: {
+        create: processNote()
+      }
     });
   });
 
-  it('runs the hook', async () => {
-    const result = await app.service('dummy').get('test');
-    
-    assert.deepEqual(result, { id: 'test' });
+  it('processes the note as expected', async () => {
+    // A user stub with just an `_id`
+    const user = { _id: 'test' };
+    // The service method call `params`
+    const params = { user };
+
+    // Create a new note with params that contains our user
+    const note = await app.service('notes').create(
+      {
+        x: 20,
+        y: 25,
+        text: 'Hi there',
+        additional: 'should be removed'
+      },
+      params
+    );
+
+    assert.equal(note.text, 'Hi there');
+    // `userId` was set
+    assert.equal(note.userId, 'test');
+    // `additional` property has been removed
+    assert.ok(!note.additional);
   });
 });
