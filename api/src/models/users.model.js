968221882723be108044caa00e82fd7d2181ae48
Timmo <contact@timmo.xyz>
2019-03-18 23:26:16 +0000

    :hammer: Allow custom db path

--- api/src/models/users.model.js
+++ api/src/models/users.model.js
@@ -1,8 +1,8 @@
 const NeDB = require('nedb');
 const path = require('path');
 
-module.exports = function (app) {
-  const dbPath = app.get('nedb');
+module.exports = function(app) {
+  const dbPath = process.env.DB_PATH || app.get('nedb');
   const Model = new NeDB({
     filename: path.join(dbPath, 'users.db'),
     autoload: true
